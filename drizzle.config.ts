9027546288289e80601a8ef58eb414c31c817e4d
Daniel-627 <ochiengdaniel627@gmail.com>
2026-02-25 11:54:41 +0300

    chore: scaffold folder structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/lib/db/schema.ts",
-  out: "./src/lib/db/migrations",
+  schema: "./app/lib/db/schema.ts",
+  out: "./app/lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
 });
-
